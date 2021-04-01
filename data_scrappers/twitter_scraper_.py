914a438f83c2be8348c45ec72e9b021124dc7e19
Jacob Hernandez <jacob.hernandez0830@gmail.com>
2021-04-01 08:33:18 -0700

    finished scrapping twitter

--- data_scrappers/twitter_scraper_.py
+++ data_scrappers/twitter_scraper_.py
@@ -1,15 +1,44 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
-from twitter_scraper import get_tweets
 from datetime import datetime, timedelta
 import datetime as dt
 import time
+import tweepy
 
+key = 'r9YGVd2pyO5U6VUccfuglgLs9'
+key_secret = 'ADhUJnWi44C2hxHwho42pCWJ4W144XIfCeVZBt8RQF4MSAIj8O'
 
+token = '740611933754167297-w9qnnOphls7EKfEQP1HhzZTWWUjbiam'
+token_secret = 'IPfJEOz8wGKYmQERAdIfSXTJLzeBPlQU1WD4o13S9y5KF'
 
-# print(tweets_df)
-# wsb_tickers = pd.read_csv('D:\\Github\\financial_dashboard\data_scrappers\\wsb_tickers.csv',names=['Tickers'],header =0)
-# wsb_tickers = wsb_tickers.sort_values(by = 'Tickers', ascending=False)
-# top_15_tickers = wsb_tickers.head(15).index
+auth = tweepy.OAuthHandler(key, key_secret)
+auth.set_access_token(token, token_secret)
+
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+wsb_tickers = pd.read_csv('D:\\Github\\financial_dashboard\data_scrappers\\wsb_tickers.csv',names=['Tickers'],header =0)
+wsb_tickers = wsb_tickers.sort_values(by = 'Tickers', ascending=False)
+top_15_tickers = wsb_tickers.head(15).index
 # top_15_comp_name = [yf.Ticker(ticker).info['shortName'].split()[0] for ticker in top_15_tickers]
+
+count = 5000
+
+df = pd.DataFrame()
+for ticker in top_15_tickers:
+    try:
+        # Creation of query method using parameters
+        tweets = tweepy.Cursor(api.search, q=ticker,count=450, lang='en',result_type='mixed').items(count)
+
+        # Pulling information from tweets iterable object
+        tweets_list = [[tweet.created_at, ticker, tweet.text] for tweet in tweets]
+
+        # Creation of dataframe from tweets list
+        # Add or remove columns as you remove tweet information
+        tweets_df = pd.DataFrame(tweets_list)
+        df = df.append(tweets_df,ignore_index=True)
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        time.sleep(3)
+
+df.to_csv('tweets.csv')

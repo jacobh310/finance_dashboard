f5c38e28acbc70b54657860674662e60364394ce
Jacob Hernandez <jacob.hernandez0830@gmail.com>
2021-04-11 14:31:22 -0700

    added path variables to data base location and twitter keys

---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+key = os.environ.get('KEY')
+key_secret = os.environ.get('SECRET')
+token = os.environ.get('TOKEN')
+token_secret = os.environ.get('TOKEN_SECRET')
+
+DATABASE_URL = os.environ['DATABASE_URL']

480649bcd130f53280626551bb8d06eb0406d632
Jacob Hernandez <jacob.hernandez0830@gmail.com>
2021-04-11 15:00:35 -0700

    updated config vars in settings

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ key_secret = os.environ.get('SECRET')
 token = os.environ.get('TOKEN')
 token_secret = os.environ.get('TOKEN_SECRET')
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ.get('DATABASE_URI')
